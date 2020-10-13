cc59c3e3c76c8b08c62ff1d25717052814723aed
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-10-13 12:11:20 +0530

    fix empty string as visible caption

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -13,7 +13,7 @@ class PropertiesController < ApplicationController
   def suggestions
     query = params[:q]
     sql = Property.search_query(query)
-    sql = sql.select('distinct COALESCE(visible_caption, suggestion) suggestion')
+    sql = sql.select('distinct COALESCE(CASE WHEN visible_caption = \'\' THEN NULL ELSE visible_caption END, suggestion) suggestion')
     render json: {
       suggestions: sql.all.map(&:suggestion)
     }
