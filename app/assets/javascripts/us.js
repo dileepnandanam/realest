2e71d3ebec4c162f0455360e0e3ada0f9122484e
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-10-22 05:50:43 +0530

    animate us

--- app/assets/javascripts/us.js
+++ app/assets/javascripts/us.js
@@ -2,4 +2,14 @@ $(document).on('turbolinks:load', function() {
   $(document).on('ajax:success', '.message-us-form-link', function(e) {
     $(this).closest('.message-form-container').html(e.detail[2].responseText)
   })
+
+  var sections = $.map($('.section'), function(e, i) {return(e)})
+
+  iterate = function(sections, i) {
+    if(i == sections.length)
+      return
+    $(sections[i]).show(100)
+    setTimeout(function(){iterate(sections, i+1)}, 100)
+  }
+  iterate(sections, 0)
 })
