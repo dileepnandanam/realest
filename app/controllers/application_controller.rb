79524aa7da97dd31422aaf03bbbac9e057dfb8b7
Dileep Nandanam <dileepsnandanam@gmail.com>
2020-11-03 07:45:29 +0530

    fix acc update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:contact_number, :name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:contact_number, :name])
   end
 
   def set_https
